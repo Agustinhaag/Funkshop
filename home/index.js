543a60c68098ffde268d7eb2dc0d54bcaa7ade0d
agustinhaag <agustin-haag@hotmail.com>
2023-06-06 18:36:29 -0300

    implementación de carrusel

--- home/index.js
+++ home/index.js
@@ -22,5 +22,40 @@ window.addEventListener("scroll", function(){
   main.classList.toggle("scroll", window.scrollY>440);
 })
 
+let fila = document.querySelector(".contenedor-proyecto");
+let carrusel = document.querySelectorAll(".carrusel");
+let derecha = document.getElementById("flecha-derecha");
+let izquierda = document.getElementById("flecha-izquierda");
 
+derecha.addEventListener("click", () => {
+  fila.scrollLeft += fila.offsetWidth;
+  let indicadoractivo = document.querySelector(".indicadores .activo");
+  if (indicadoractivo && indicadoractivo.nextElementSibling) {
+    indicadoractivo.nextElementSibling.classList.add("activo");
+    indicadoractivo.classList.remove("activo");
+  }
+});
+
+izquierda.addEventListener("click", () => {
+  fila.scrollLeft -= fila.offsetWidth;
+  let indicadoractivo = document.querySelector(".indicadores .activo");
+  if (indicadoractivo && indicadoractivo.previousElementSibling) {
+    indicadoractivo.previousElementSibling.classList.add("activo");
+    indicadoractivo.classList.remove("activo");
+  }
+});
+
+const numpag = Math.ceil(carrusel.length / 3);
+for (let i = 0; i < numpag; i++) {
+  const indicador = document.createElement("button");
+  if (i === 0) {
+    indicador.classList.add("activo");
+  }
+  document.querySelector(".indicadores").appendChild(indicador);
+  indicador.addEventListener("click", (e) => {
+    fila.scrollLeft = i * fila.offsetWidth;
+    document.querySelector(".indicadores .activo").classList.remove("activo");
+    e.target.classList.add("activo");
+  });
+}
 
--- item/item.js
+++ item/item.js
@@ -10,7 +10,8 @@ function restar (){
     contador= contador -1;
     cantidad.value= contador;
    if(cantidad.value<0){
-    contador=0;
+     contador = 0;
+     cantidad.value = 0;
    }
 }
 let menu = document.getElementById("menu");
@@ -35,6 +36,43 @@ const main = document.getElementById("nav");
 window.addEventListener("scroll", function(){
   main.classList.toggle("scroll", window.scrollY>0);
 })
+
+let fila = document.querySelector(".contenedor-proyecto");
+let carrusel = document.querySelectorAll(".carrusel");
+let derecha = document.getElementById("flecha-derecha");
+let izquierda = document.getElementById("flecha-izquierda");
+
+derecha.addEventListener("click", () => {
+  fila.scrollLeft += fila.offsetWidth;
+  let indicadoractivo = document.querySelector(".indicadores .activo");
+  if (indicadoractivo && indicadoractivo.nextElementSibling) {
+    indicadoractivo.nextElementSibling.classList.add("activo");
+    indicadoractivo.classList.remove("activo");
+  }
+});
+
+izquierda.addEventListener("click", () => {
+  fila.scrollLeft -= fila.offsetWidth;
+  let indicadoractivo = document.querySelector(".indicadores .activo");
+  if (indicadoractivo && indicadoractivo.previousElementSibling) {
+    indicadoractivo.previousElementSibling.classList.add("activo");
+    indicadoractivo.classList.remove("activo");
+  }
+});
+
+const numpag = Math.ceil(carrusel.length / 3);
+for (let i = 0; i < numpag; i++) {
+  const indicador = document.createElement("button");
+  if (i === 0) {
+    indicador.classList.add("activo");
+  }
+  document.querySelector(".indicadores").appendChild(indicador);
+  indicador.addEventListener("click", (e) => {
+    fila.scrollLeft = i * fila.offsetWidth;
+    document.querySelector(".indicadores .activo").classList.remove("activo");
+    e.target.classList.add("activo");
+  });
+}
 
 
 
